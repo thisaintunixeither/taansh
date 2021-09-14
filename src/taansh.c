d14ba14f84d44aa99b11e5f80157c3b4d3e68cf1
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-14 08:23:49 -0700

    Added more (that I'm hoping won't segfault bc I haven't tested)

--- src/taansh.c
+++ src/taansh.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int taansh(void) {
   printf("We jumped up on the table\n");
-  printf("And shouted \"ANARCHY!\"\n");
+  printf("And shouted \"ANARCHY!\"\n\n\n");
+  printf("\t\t Welcome to TAANSH: This Ain't A Normal SHell");
+  exit(EXIT_SUCCESS);
 }
