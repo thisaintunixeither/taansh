814be6b28954a624e61bc828f95241af9fbfbcd2
GNUOverYooouu <dtortoise99@gmail.com>
2021-09-15 13:31:15 -0700

    Changed stuff

--- taansh.c
+++ taansh.c
@@ -6,9 +6,9 @@
 char *builtins[6] = { "help", "hello", "deadmilkmen", "cd", "pwd", "exit" };
 
 int taansh(void) {
-  fork();
   printf("We jumped up on the table\n");
   printf("And shouted \"ANARCHY!\"\n\n\n");
-  printf("\t\t Welcome to TAANSH: This Ain't A Normal SHell");
+  printf("\t\t Welcome to TAANSH: This Ain't A Normal SHell\n");
+  printf("youcanttypeanythingyet: ");
   exit(EXIT_SUCCESS);
 }
